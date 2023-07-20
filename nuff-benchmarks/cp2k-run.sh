bcab4e4d2fafdfab9cb122eecc01c82a0fde85c8
SpySappingMySentry <matthewroy31@gmail.com>
2023-07-20 14:43:10 +0100

    Proper run of H2O-64

---
+++ nuff-benchmarks/cp2k-run.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#SBATCH --partition=standard
+#SBATCH --qos=standard
+#SBATCH --time=00:05:00
+#SBATCH --exclusive
+#SBATCH --nodes=1
+#SBATCH --ntasks=36
+#SBATCH --tasks-per-node=36
+#SBATCH --cpus-per-task=1
+
+module load cp2k
+
+export OMP_NUM_THREADS=36
+
+srun --cpu-bind=cores cp2k.psmp -i ~/benchmarks/QS/H2O-64.inp -o ~/nuff-benchmarks/cp2k-output.txt
