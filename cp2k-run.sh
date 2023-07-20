b330fb81ea4892ce582243c0c23066d2c32d2f36
not populated <mrnuff-cirrus@cirrus-login1.ib0.icexa.epcc.ed.ac.uk>
2023-07-20 13:56:00 +0100

    Re-ran H2O-64

--- cp2k-run.sh
+++ cp2k-run.sh
@@ -3,11 +3,14 @@
 #SBATCH --partition=standard
 #SBATCH --qos=standard
 #SBATCH --time=00:05:00
-
-#SBATCH --nodes=1
-#SBATCH --ntasks-per-node=16
+#SBATCH --exclusive
+#SBATCH --nodes=4
+#SBATCH --ntasks=8
+#SBATCH --tasks-per-node=2
+#SBATCH --cpus-per-task=18
 
 module load cp2k
 
-cp2k.psmp -i /work/tc047/tc047/mrnuff-cirrus/cp2k/benchmarks/QS/H2O-64.inp -o cp2k-output.txt
+export OMP_NUM_THREADS=18
 
+srun --cpu-bind=cores cp2k.psmp -i /work/tc047/tc047/mrnuff-cirrus/cp2k/benchmarks/QS/H2O-64.inp -o /work/tc047/tc047/mrnuff-cirrus/cp2k/nuff-benchmarks/cp2k-output.txt
