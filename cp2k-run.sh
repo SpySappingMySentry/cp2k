cb867113693baba730077845c0486233ae8ea100
SpySappingMySentry <matthewroy31@gmail.com>
2023-07-21 11:25:54 +0100

    2 node run of H2O-64

--- cp2k-run.sh
+++ cp2k-run.sh
@@ -5,12 +5,12 @@
 #SBATCH --time=00:05:00
 #SBATCH --exclusive
 #SBATCH --nodes=2
-#SBATCH --ntasks=72
-#SBATCH --tasks-per-node=36
-#SBATCH --cpus-per-task=1
+#SBATCH --ntasks=18
+#SBATCH --tasks-per-node=9
+#SBATCH --cpus-per-task=2
 
 module load cp2k
 
-export OMP_NUM_THREADS=1
+export OMP_NUM_THREADS=2
 
 srun --cpu-bind=cores cp2k.psmp -i /work/tc047/tc047/mrnuff-cirrus/cp2k/benchmarks/QS/H2O-64.inp -o H2O-64-output.txt
