5dbb397d2d1953d13bb66f40655d52d6d26c440a
SpySappingMySentry <matthewroy31@gmail.com>
2023-07-21 14:03:09 +0100

    Ran H2O-32

--- cp2k-run.sh
+++ cp2k-run.sh
@@ -4,13 +4,13 @@
 #SBATCH --qos=standard
 #SBATCH --time=00:05:00
 #SBATCH --exclusive
-#SBATCH --nodes=2
+#SBATCH --nodes=1
 #SBATCH --ntasks=36
-#SBATCH --tasks-per-node=18
-#SBATCH --cpus-per-task=2
+#SBATCH --tasks-per-node=36
+#SBATCH --cpus-per-task=1
 
 module load cp2k
 
-export OMP_NUM_THREADS=2
+export OMP_NUM_THREADS=1
 
-srun --cpu-bind=cores cp2k.psmp -i /work/tc047/tc047/mrnuff-cirrus/cp2k/benchmarks/QS/H2O-64.inp -o H2O-64-output.txt
+srun --cpu-bind=cores cp2k.psmp -i benchmarks/QS/H2O-32.inp -o H2O-32-output.txt
