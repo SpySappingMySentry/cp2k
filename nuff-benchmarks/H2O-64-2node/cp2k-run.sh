b22740a482803f0dff5ff7a70a2d9185eeb8bf38
SpySappingMySentry <matthewroy31@gmail.com>
2023-07-21 13:15:14 +0100

    Moved 2 core run script

---
+++ nuff-benchmarks/H2O-64-2node/cp2k-run.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#SBATCH --partition=standard
+#SBATCH --qos=standard
+#SBATCH --time=00:05:00
+#SBATCH --exclusive
+#SBATCH --nodes=2
+#SBATCH --ntasks=36
+#SBATCH --tasks-per-node=18
+#SBATCH --cpus-per-task=2
+
+module load cp2k
+
+export OMP_NUM_THREADS=2
+
+srun --cpu-bind=cores cp2k.psmp -i /work/tc047/tc047/mrnuff-cirrus/cp2k/benchmarks/QS/H2O-64.inp -o H2O-64-output.txt
