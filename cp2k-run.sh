8cb435207d6a1f6a60999c3741e60cb0885a44a6
SpySappingMySentry <matthewroy31@gmail.com>
2023-07-24 14:07:01 +0100

    4 node run of H2O-64 with 1 OMP thread

--- cp2k-run.sh
+++ cp2k-run.sh
@@ -4,13 +4,13 @@
 #SBATCH --qos=standard
 #SBATCH --time=00:05:00
 #SBATCH --exclusive
-#SBATCH --nodes=2
-#SBATCH --ntasks=8
-#SBATCH --tasks-per-node=4
-#SBATCH --cpus-per-task=9
+#SBATCH --nodes=4
+#SBATCH --ntasks=144
+#SBATCH --tasks-per-node=36
+#SBATCH --cpus-per-task=1
 
 module load cp2k
 
-export OMP_NUM_THREADS=9
+export OMP_NUM_THREADS=1
 
 srun --cpu-bind=cores cp2k.psmp -i benchmarks/QS/H2O-64.inp -o H2O-64-output.txt
