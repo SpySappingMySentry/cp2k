20faf4c0683c1b67908bc46dc71cbd4dce06aa1c
SpySappingMySentry <matthewroy31@gmail.com>
2023-07-27 13:26:02 +0100

    Re-runs of 2 node runs to improve accuracy of results

--- nuff-benchmarks/H2O-64-2node/cp2k-run.sh
+++ nuff-benchmarks/H2O-64-2node/cp2k-run.sh
@@ -4,13 +4,13 @@
 #SBATCH --qos=standard
 #SBATCH --time=00:05:00
 #SBATCH --exclusive
-#SBATCH --nodes=1
-#SBATCH --ntasks=18
-#SBATCH --tasks-per-node=18
-#SBATCH --cpus-per-task=2
+#SBATCH --nodes=2
+#SBATCH --ntasks=8
+#SBATCH --tasks-per-node=4
+#SBATCH --cpus-per-task=9
 
 module load cp2k
 
-export OMP_NUM_THREADS=2
+export OMP_NUM_THREADS=9
 
 srun --cpu-bind=cores cp2k.psmp -i /work/tc047/tc047/mrnuff-cirrus/cp2k/benchmarks/QS/H2O-64.inp -o H2O-64-output.txt
