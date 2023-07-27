7bd6e17a20da59fb7d897e2bf669b14493ff908d
SpySappingMySentry <matthewroy31@gmail.com>
2023-07-27 11:11:54 +0100

    Re-ran 1 node runs to find more accurate results

--- cp2k-run.sh
+++ cp2k-run.sh
@@ -4,13 +4,13 @@
 #SBATCH --qos=standard
 #SBATCH --time=00:05:00
 #SBATCH --exclusive
-#SBATCH --nodes=4
-#SBATCH --ntasks=8
-#SBATCH --tasks-per-node=2
-#SBATCH --cpus-per-task=18
+#SBATCH --nodes=1
+#SBATCH --ntasks=36
+#SBATCH --tasks-per-node=36
+#SBATCH --cpus-per-task=1
 
 module load cp2k
 
-export OMP_NUM_THREADS=18
+export OMP_NUM_THREADS=1
 
 srun --cpu-bind=cores cp2k.psmp -i benchmarks/QS/H2O-64.inp -o H2O-64-output.txt
--- nuff-benchmarks/H2O-64-1node/cp2k-run.sh
+++ nuff-benchmarks/H2O-64-1node/cp2k-run.sh
@@ -5,12 +5,12 @@
 #SBATCH --time=00:05:00
 #SBATCH --exclusive
 #SBATCH --nodes=1
-#SBATCH --ntasks=36
-#SBATCH --tasks-per-node=36
-#SBATCH --cpus-per-task=1
+#SBATCH --ntasks=9
+#SBATCH --tasks-per-node=9
+#SBATCH --cpus-per-task=4
 
 module load cp2k
 
-export OMP_NUM_THREADS=1
+export OMP_NUM_THREADS=9
 
 srun --cpu-bind=cores cp2k.psmp -i /work/tc047/tc047/mrnuff-cirrus/cp2k/benchmarks/QS/H2O-64.inp -o H2O-64-output.txt
