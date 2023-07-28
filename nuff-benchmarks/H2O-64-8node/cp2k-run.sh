88c3052504e4a622b1eb01f0e38f2b6913bd9d99
SpySappingMySentry <matthewroy31@gmail.com>
2023-07-28 11:02:17 +0100

    Runs of H2O-64 using 8 nodes

--- nuff-benchmarks/H2O-64-8node/cp2k-run.sh
+++ nuff-benchmarks/H2O-64-8node/cp2k-run.sh
@@ -4,13 +4,13 @@
 #SBATCH --qos=standard
 #SBATCH --time=00:05:00
 #SBATCH --exclusive
-#SBATCH --nodes=4
-#SBATCH --ntasks=16
+#SBATCH --nodes=8
+#SBATCH --ntasks=32
 #SBATCH --tasks-per-node=4
 #SBATCH --cpus-per-task=9
 
 module load cp2k
 
-export OMP_NUM_THREADS=6
+export OMP_NUM_THREADS=9
 
 srun --cpu-bind=cores cp2k.psmp -i /work/tc047/tc047/mrnuff-cirrus/cp2k/benchmarks/QS/H2O-64.inp -o H2O-64-output-9OMP-4.txt
