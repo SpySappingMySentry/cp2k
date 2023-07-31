515500afef7a7fda63c782d3477f3fd3ff9b0d1a
SpySappingMySentry <matthewroy31@gmail.com>
2023-07-31 11:10:47 +0100

    Test run of fayalite

---
+++ nuff-benchmarks/fayalite/cp2k-run.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#SBATCH --partition=standard
+#SBATCH --qos=standard
+#SBATCH --time=00:05:00
+#SBATCH --exclusive
+#SBATCH --nodes=1
+#SBATCH --ntasks=36
+#SBATCH --tasks-per-node=36
+#SBATCH --cpus-per-task=1
+
+module load cp2k
+
+export OMP_NUM_THREADS=1
+
+srun --cpu-bind=cores cp2k.psmp -i /work/tc047/tc047/mrnuff-cirrus/cp2k/benchmarks/Fayalite-FIST/fayalite.inp -o fayalite-output-1OMP-1.txt
