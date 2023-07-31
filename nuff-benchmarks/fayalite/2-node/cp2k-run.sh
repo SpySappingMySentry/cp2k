42636083e38daa000d6d76adae407ba694219c03
SpySappingMySentry <matthewroy31@gmail.com>
2023-07-31 12:51:42 +0100

    All runs of fayalite

---
+++ nuff-benchmarks/fayalite/2-node/cp2k-run.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#SBATCH --partition=standard
+#SBATCH --qos=standard
+#SBATCH --time=00:05:00
+#SBATCH --exclusive
+#SBATCH --nodes=2
+#SBATCH --ntasks=8
+#SBATCH --tasks-per-node=4
+#SBATCH --cpus-per-task=9
+
+module load cp2k
+
+export OMP_NUM_THREADS=9
+
+srun --cpu-bind=cores cp2k.psmp -i /work/tc047/tc047/mrnuff-cirrus/cp2k/benchmarks/Fayalite-FIST/fayalite.inp -o fayalite-output-9OMP-2.txt
---
+++ nuff-benchmarks/fayalite/4-node/cp2k-run.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#SBATCH --partition=standard
+#SBATCH --qos=standard
+#SBATCH --time=00:05:00
+#SBATCH --exclusive
+#SBATCH --nodes=4
+#SBATCH --ntasks=16
+#SBATCH --tasks-per-node=4
+#SBATCH --cpus-per-task=9
+
+module load cp2k
+
+export OMP_NUM_THREADS=9
+
+srun --cpu-bind=cores cp2k.psmp -i /work/tc047/tc047/mrnuff-cirrus/cp2k/benchmarks/Fayalite-FIST/fayalite.inp -o fayalite-output-9OMP-4.txt
