276f0bbcd4f568cda35802dc4c6aa106cebbfe09
SpySappingMySentry <matthewroy31@gmail.com>
2023-08-04 12:20:34 +0100

    Re-runs of fayalite with added time

--- nuff-benchmarks/fayalite/1-node/cp2k-run.sh
+++ nuff-benchmarks/fayalite/1-node/cp2k-run.sh
@@ -2,20 +2,19 @@
 
 #SBATCH --partition=standard
 #SBATCH --qos=standard
-#SBATCH --time=00:05:00
+#SBATCH --time=00:10:00
 #SBATCH --exclusive
 #SBATCH --nodes=1
-#SBATCH --ntasks=4
-#SBATCH --tasks-per-node=4
-#SBATCH --cpus-per-task=9
+#SBATCH --ntasks=36
+#SBATCH --tasks-per-node=36
+#SBATCH --cpus-per-task=1
 
 my_cp2k_bin=/work/tc047/tc047/mrnuff-cirrus/cp2k/cp2k-9.1/exe/Cirrus-intel
 
-export OMP_NUM_THREADS=9
+export OMP_NUM_THREADS=1
 export LD_LIBRARY_PATH=/work/tc047/tc047/mrnuff-cirrus/cp2k/cp2k-9.1/libs/elpa-openmp/lib:$LD_LIBRARY_PATH
 
 echo $LD_LIBRARY_PATH
 
 module load intel-20.4/compilers intel-20.4/mpi intel-20.4/cmkl
-
-srun --cpu-bind=cores $my_cp2k_bin/cp2k.psmp -i /work/tc047/tc047/mrnuff-cirrus/cp2k/benchmarks/Fayalite-FIST/fayalite.inp -o fayalite-output-9OMP-8.txt
+srun --cpu-bind=cores $my_cp2k_bin/cp2k.psmp -i /work/tc047/tc047/mrnuff-cirrus/cp2k/benchmarks/Fayalite-FIST/fayalite.inp -o fayalite-output-1OMP-8.txt
